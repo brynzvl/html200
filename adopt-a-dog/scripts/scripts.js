69cd4634819b89042f98c780cb314454c2c3f07e
Brayan Zavala <brynzvl93@gmail.com>
2019-02-12 02:35:54 -0800

    updated and formated html and js

--- adopt-a-dog/scripts/scripts.js
+++ adopt-a-dog/scripts/scripts.js
@@ -1,13 +1,15 @@
 let message;
+
 function dogInfo(name, breed, price) {
-  message = (name + " is a " + breed + ". "  +  "Total adoption fees come to " + price);
-  return message;
+	message = (name + " is a " + breed + ". " + "Total adoption fees come to " + price);
+	return message;
 }
 
 let adoption;
+
 function calCost(totalCost) {
-  adoption = ("The total cost for adoption is " + totalCost + '.');
-  return adoption;
-  console.log(adoption)
+	adoption = ("The total cost for adoption is " + totalCost + '.');
+	return adoption;
+	console.log(adoption)
 
 }
