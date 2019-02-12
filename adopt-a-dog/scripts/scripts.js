3ed370509ae70dacc274a9dff7eb02b95bef481b
Brayan Zavala <brynzvl93@gmail.com>
2019-02-12 01:54:39 -0800

    added .js file and updated grids on index page

---
+++ adopt-a-dog/scripts/scripts.js
@@ -0,0 +1,13 @@
+let message;
+function dogInfo(name, breed, price) {
+  message = (name + " is a " + breed + ". "  +  "Total adoption fees come to " + price);
+  return message;
+}
+
+let adoption;
+function calCost(totalCost) {
+  adoption = ("The total cost for adoption is " + totalCost + '.');
+  return adoption;
+  console.log(adoption)
+
+}
