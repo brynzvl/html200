7cfced58b1c59ee9d7410099aa4522f52c82dcca
Brayan Zavala <brynzvl93@gmail.com>
2019-03-25 22:05:41 -0700

    edited and made corrections to code.

--- adopt-a-dog/scripts/jquery.js
+++ adopt-a-dog/scripts/jquery.js
@@ -1,10 +1,12 @@
+$(function() {
+  "use strict";
 
-$('.dog-profile').hover(function(e) {
-  $('.dog-profile').toggleClass('dog-profile-red');
-  console.log('Hello');
+$('dog-profile').hover(function(e) {
+  $('dog-profile').toggleClass('dog-profile-red');
 })
 
 $('.dog-page-profile').hover(function(e) {
   $('.dog-page-profile').toggleClass('dog-profile-red');
-  console.log('Hello');
 })
+
+});
